1e22c1295f1a42cef2fc009426b3ceeb374c55c3
Harsh Shinde <Harsh.Shinde@thomsonreuters.com>
2025-05-28 11:12:20 +0530

    Sumarry report Consolidation report Workflow changes SCD CIAM Login Updated Wizards

---
+++ .codedworkflows/ConnectionsFactory.cs
@@ -0,0 +1,6 @@
+using UiPath.CodedWorkflows;
+using System;
+
+namespace TR_AUT_API_Submission_Performer
+{
+}
---
+++ .codedworkflows/ConnectionsManager.cs
@@ -0,0 +1,12 @@
+using UiPath.CodedWorkflows;
+using System;
+
+namespace TR_AUT_API_Submission_Performer
+{
+    public class ConnectionsManager
+    {
+        public ConnectionsManager(ICodedWorkflowsServiceContainer resolver)
+        {
+        }
+    }
+}
